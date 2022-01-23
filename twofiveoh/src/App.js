57f051ace726f5f9f170031d5b9d8db4150d331a
jonathan <uuupah@gmail.com>
2022-01-23 21:56:21 +1030

    add header links and date disabling

--- twofiveoh/src/App.js
+++ twofiveoh/src/App.js
@@ -4,8 +4,19 @@ import React from "react";
 import { ReactComponent as TfoLogo } from "./assets/svg/logo2.svg";
 import { ReactComponent as StarSVG } from "./assets/svg/star.svg";
 import { ReactComponent as IMDBSVG } from "./assets/svg/imdb.svg";
+import { ReactComponent as InstaSVG} from "./assets/svg/insta.svg"
+import { ReactComponent as SpotifySVG} from "./assets/svg/spotify.svg"
+import { ReactComponent as AppleSVG} from "./assets/svg/apple.svg"
+import { ReactComponent as MailSVG} from "./assets/svg/mail.svg"
 
 let epdata = require("./data/list.json");
+let sevenjantwenty = new Date(2020, 0, 7);
+
+function addDays(days) {
+  const copy = new Date(sevenjantwenty);
+  copy.setDate(sevenjantwenty.getDate() + days);
+  return copy;
+}
 
 function WhooshkaEmbed(props) {
   return (
@@ -30,6 +41,12 @@ function Header(props) {
   return (
     <div className="header">
       <TfoLogo width="250px" />
+      <ul className="links">
+        <li><a href="https://www.instagram.com/twofiveohpod/"><InstaSVG height="1em" className="svg"/> <span className="paddedspan">instagram</span></a></li>
+        <li><a href="https://podcasts.apple.com/kz/podcast/twofiveoh/id1493890285"><AppleSVG height="1em" className="svg"/> <span className="paddedspan">apple</span></a></li>
+        <li><a href="https://open.spotify.com/show/39lr9bBUcXgZRXsxTw1axM"><SpotifySVG height="1em" className="svg"/> <span className="paddedspan">spotify</span></a></li>
+        <li><a href="mailto:twofiveohpod@gmail.com"><MailSVG height="1em" className="svg"/> <span className="paddedspan">mail</span></a></li>
+      </ul>
     </div>
   );
 }
@@ -37,8 +54,9 @@ function Header(props) {
 function Episodelist(props) {
   return (
     <div className="episodelist">
-      {epdata.map((ep) => (
+      {epdata.map((ep, index) => (
         <Episode
+          index={index}
           epindex={ep.epindex}
           tfoindex={ep.tfoindex}
           imdbid={ep.imdbid}
@@ -57,7 +75,7 @@ function Episodelist(props) {
 function Episode(props) {
   return (
     <div className="episode">
-      <div className="episodecontents">
+      <div className={"episodecontents" + String(new Date() > addDays(props.index * 7) ? "" : " disabled")} >
         <img src={props.coverurl} className="poster" />
         <div>
           <p className="episodetitle">
