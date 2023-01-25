94cefe8946e4724358414a63d6f65f668698afca
Jonathan Souter <Jonathan.Souter@atturra.com>
2023-01-25 11:25:47 +1030

    iterate through 7 batches of episodes to get all data

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+const clientId = SPOTIFY_CLIENT_ID;
+
+const clientSecret = SPOTIFY_CLIENT_SECRET;
+
+const showId = "39lr9bBUcXgZRXsxTw1axM";
+const requestLimit = 50;
+const requestOffset = 0;
+
+const spotifyTokenEndpoint = "https://accounts.spotify.com/api/token";
+// note that the market is absolutely fucking VITAL here. despite it being optional, the
+// endpoint returns a 404 without it
+const spotifyEpisodesEndpoint = `https://api.spotify.com/v1/shows/${showId}/episodes?market=AU&limit=${String(requestLimit)}&offset=`
+
 /**
- * Respond with hello worker text
+ * respond with json of spotify info
  * @param {Request} request
 */
 async function handleRequest(request) {
@@ -10,17 +23,6 @@ async function handleRequest(request) {
   // https://developer.spotify.com/console/get-show-episodes/?id=39lr9bBUcXgZRXsxTw1axM&market=&limit=&offset=
   // if one is needed for development
   // const bearerToken = "";
-  const clientId = SPOTIFY_CLIENT_ID;
-  const clientSecret = SPOTIFY_CLIENT_SECRET;
-
-  const showId = "39lr9bBUcXgZRXsxTw1axM";
-  const requestLimit = 50;
-  const requestOffset = 0;
-
-  const spotifyTokenEndpoint = "https://accounts.spotify.com/api/token";
-  // note that the market is absolutely fucking VITAL here. despite it being optional, the
-  // endpoint returns a 404 without it
-  const spotifyEpisodesEndpoint = `https://api.spotify.com/v1/shows/${showId}/episodes?market=AU&limit=${String(requestLimit)}&offset=${String(requestOffset)}`
 
   const bodyParams = new URLSearchParams()
   bodyParams.append("grant_type", 'client_credentials')
@@ -44,11 +46,21 @@ async function handleRequest(request) {
 
   var { access_token } = await getAccessToken();
 
-  return response = await fetch(spotifyEpisodesEndpoint, {
-    headers: {
-      'Authorization': `Bearer ${access_token}`
-    },
-  })
+  var episodes = [];
+
+  for (let i = 0; i < 7; i++) {
+    var response = await fetch(spotifyEpisodesEndpoint + String(50 * i), {
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      },
+    })
+
+    var jsonResponse = await response.json();
+    var contents = JSON.parse(JSON.stringify(jsonResponse)).items;
+
+    episodes = episodes.concat(contents)
+  }
+  return new Response(JSON.stringify(episodes), response);
 }
 
-      // https://github.com/BehnH/spotify-workers/blob/main/src/utils/spotify.js
+// https://github.com/BehnH/spotify-workers/blob/main/src/utils/spotify.js
